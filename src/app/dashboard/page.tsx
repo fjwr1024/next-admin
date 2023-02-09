db9f09de2ad2282254962ae32259b4bf02d70c78
FujiwaraKen <fjwr1024@gmail.com>
2023-02-09 17:49:13 +0900

    split global layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -33,64 +33,46 @@ const mdTheme = createTheme()
 
 function DashboardContent() {
   return (
-    <ThemeProvider theme={mdTheme}>
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <HeaderSideMenu />
-        <Box
-          component='main'
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === 'light' ? theme.palette.grey[100] : theme.palette.grey[900],
-            flexGrow: 1,
-            height: '100vh',
-            overflow: 'auto',
-          }}
-        >
-          <Toolbar />
-          <Container maxWidth='lg' sx={{ mt: 4, mb: 4 }}>
-            <SystemWalletInfo />
-            <Box mt={3}>
-              <Grid container spacing={3}>
-                {/* Chart */}
-                <Grid item xs={12} md={8} lg={9}>
-                  <Paper
-                    sx={{
-                      p: 2,
-                      display: 'flex',
-                      flexDirection: 'column',
-                      height: 240,
-                    }}
-                  >
-                    <Chart />
-                  </Paper>
-                </Grid>
-                {/* Recent Deposits */}
-                <Grid item xs={12} md={4} lg={3}>
-                  <Paper
-                    sx={{
-                      p: 2,
-                      display: 'flex',
-                      flexDirection: 'column',
-                      height: 240,
-                    }}
-                  >
-                    <Deposits />
-                  </Paper>
-                </Grid>
-                {/* Recent Orders */}
-                <Grid item xs={12}>
-                  <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                    <Orders />
-                  </Paper>
-                </Grid>
-              </Grid>
-              <Copyright sx={{ pt: 4 }} />
-            </Box>
-          </Container>
-        </Box>
+    <>
+      <SystemWalletInfo />
+      <Box mt={3}>
+        <Grid container spacing={3}>
+          {/* Chart */}
+          <Grid item xs={12} md={8} lg={9}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                height: 240,
+              }}
+            >
+              <Chart />
+            </Paper>
+          </Grid>
+          {/* Recent Deposits */}
+          <Grid item xs={12} md={4} lg={3}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                height: 240,
+              }}
+            >
+              <Deposits />
+            </Paper>
+          </Grid>
+          {/* Recent Orders */}
+          <Grid item xs={12}>
+            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+              <Orders />
+            </Paper>
+          </Grid>
+        </Grid>
+        <Copyright sx={{ pt: 4 }} />
       </Box>
-    </ThemeProvider>
+    </>
   )
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,45 @@
+'use client'
+
 import '@/styles/globals.css'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+import { Box, Container, CssBaseline, Toolbar } from '@mui/material'
 import { FC, PropsWithChildren } from 'react'
+import HeaderSideMenu from './components/Menu/HeaderSideMenu'
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
+  const mdTheme = createTheme()
+
   return (
     <html lang='ja'>
       <head>
         <title>NextAdminExample</title>
       </head>
-      <body>{children}</body>
+      <body>
+        <ThemeProvider theme={mdTheme}>
+          <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <HeaderSideMenu />
+            <Box
+              component='main'
+              sx={{
+                backgroundColor: (theme) =>
+                  theme.palette.mode === 'light'
+                    ? theme.palette.grey[100]
+                    : theme.palette.grey[900],
+                flexGrow: 1,
+                height: '100vh',
+                overflow: 'auto',
+              }}
+            >
+              <Toolbar />
+              <Container maxWidth='lg' sx={{ mt: 4, mb: 4 }}>
+                {children}
+              </Container>
+            </Box>
+          </Box>
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
